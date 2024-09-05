698afd37c92293c327e2c43cc85a120d185707cc
Oreste NGABO <orestengabo3@gmail.com>
2024-09-05 12:59:21 -0700

    Removing the environment variables

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,26 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import path from 'path'
 import { connectDB } from './config/db.js'
 import productRoutes from './routes/product.router.js'
 
 dotenv.config()
 
+const app = express()
 const PORT = process.env.PORT || 4000
 
-const app = express()
-app.use(express.json())
+const __dirname = path.resolve();
 
+app.use(express.json())
 app.use('/products', productRoutes)
+
+if(process.env.NODE_ENV==="production"){
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend","dist","index.html"))
+    })
+}
 
 app.listen(5000, () => {
     connectDB()
